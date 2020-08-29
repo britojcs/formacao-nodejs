1f47bee7a92ac3d5634d37248de3ee2f532ad30e
Brito <brito@Britos-MacBook-Pro.local>
2020-08-29 17:52:58 -0300

    iniciando app perguntas e respostas

---
+++ 2-perguntas-e-respostas/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send(`Título: ${titulo}\nDescrição: ${descricao}`);
+});
+
+app.listen(3000, () => console.log("Server running"));
